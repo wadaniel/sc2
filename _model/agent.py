4a2a97af0f799e2a7406f1148f90f29505d4f316
wadaniel <wadaniel@ethz.ch>
2021-11-24 09:46:57 +0100

    init

---
+++ _model/agent.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+from smac.env import StarCraft2Env
+import numpy as np
+import math
+
+def initEnvironment(e, mapName, multPolicies):
+ 
+  env = StarCraft2Env(map_name="3m")
+  env_info = env.get_env_info()
+
+  actionVariableCount = 1
+  numPossibleActions = env_info["n_actions"]
+  possibleActions = [ [idx] for idx in range(numPossibleActions) ]
+  numIndividuals = env_info["n_agents"]
+  stateVariableCount = env_info["state_shape"]
+  observationVariableCount = env_info["obs_shape"]
+
+  print(numPossibleActions)
+  print(possibleActions)
+  print(numIndividuals)
+  print(stateVariableCount)
+  print(observationVariableCount)
+
+  ### Defining problem configuration for pettingZoo environments
+  e["Problem"]["Type"] = "Reinforcement Learning / Discrete"
+  e["Problem"]["Environment Function"] = lambda s : environment(s, env)
+  e["Problem"]["Custom Settings"]["Print Step Information"] = "Disabled"
+  e["Problem"]["Training Reward Threshold"] = math.inf
+  e["Problem"]["Possible Actions"] = possibleActions
+  e["Problem"]["Testing Frequency"] = 100
+  e["Problem"]["Policy Testing Episodes"] = 10
+  e["Problem"]["Agents Per Environment"] = numIndividuals
+  if (multPolicies == 1) :
+      e["Problem"]["Policies Per Environment"] = numIndividuals
+ 
+  # Defining State Variables
+ 
+  for i in range(observationVariableCount):
+     e["Variables"][i]["Name"] = "State Variable " + str(i)
+     e["Variables"][i]["Type"] = "State"
+  
+  # Defining Action Variables
+ 
+  for i in range(actionVariableCount):
+    e["Variables"][observationVariableCount + i]["Name"] = "Action Variable " + str(i)
+    e["Variables"][observationVariableCount + i]["Type"] = "Action"
+
+def getAvailableActions(nAgents, env):
+    availableActions = []
+    for agentId in range(nAgents):
+        availableActions.append(env.get_avail_agent_actions(agentId))
+
+    return availableActions
+
+def environment(s, env):
+        
+    env.reset()
+    env_info = env.get_env_info()
+    nAgents = env_info["n_agents"]
+    
+    terminated = False
+    episodeReward = 0
+ 
+    obs = env.get_obs()
+    obs = [ o.tolist() for o in obs ]
+    #state = env.get_state()
+
+    s["State"] = obs
+    s["Available Actions"] = getAvailableActions(nAgents, env)
+    
+    step = 0
+    while not terminated:
+        
+        s.update()
+
+        actions = s["Action"]
+        actions = [a[0] for a in actions]
+        reward, terminated, _ = env.step(actions)
+        episodeReward += reward
+ 
+        obs = env.get_obs()
+        obs = [ o.tolist() for o in obs ]
+        #state = env.get_state()
+        s["Reward"] = [ reward ] * nAgents
+ 
+        s["State"] = obs
+        s["Available Actions"] = getAvailableActions(nAgents, env)
+        step = step + 1
+ 
+    
+    s["Termination"] = "Terminal"
+    print("Total reward after {} steps : {}".format(step, episodeReward))
+    
+    #if s["Mode"] == "Testing":
+    #env.close()
+
---
+++ run-dvracer.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+sys.path.append('_model')
+from agent import *
+import pdb
+
+####### Parsing arguments
+
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', help='Specifies the starctaft map.', required=True)
+parser.add_argument('--l2', help='L2 Regularization.', required=False, type=float, default = 0.)
+parser.add_argument('--lr', help='Learning Rate.', required=False, type=float, default = 0.0001)
+parser.add_argument('--run', help='Run Number', required=True, type=int, default = 0)
+parser.add_argument('--multpolicies', help='If set to 1, train with N policies', required=False, type=int, default = 0)
+parser.add_argument('--model', help='Model Number', required=True, type=int)
+#model '0' or '' weakly Dependent Individualist 
+#model '1' strongly Dependent Individualist I 
+#model '2' strongly Dependent Individualist II 
+#model '3' weakly Dependent Collectivist  
+#model '4' strongly Dependent Collectivist I 
+#model '5' strongly Dependent Collectivist II 
+
+args = parser.parse_args()
+print(args)
+
+####### Defining Korali Problem
+
+import korali
+k = korali.Engine()
+e = korali.Experiment()
+
+### Defining results folder and loading previous results, if any
+
+resultFolder = 'results/_result_dvracer_' + args.env + '_' + str(args.model) + '_' + str(args.run) +'/'
+#e.loadState(resultFolder + '/latest');
+
+### Initializing openAI Gym environment
+
+initEnvironment(e, args.env, args.multpolicies)
+
+### Defining Agent Configuration 
+
+e["Solver"]["Type"] = "Agent / Discrete / dVRACER"
+e["Solver"]["Mode"] = "Training"
+e["Solver"]["Episodes Per Generation"] = 10
+e["Solver"]["Experiences Between Policy Updates"] = 1
+e["Solver"]["Learning Rate"] = args.lr
+e["Solver"]["Discount Factor"] = 0.995
+e["Solver"]["Mini Batch"]["Size"] = 256
+e["Solver"]["Multi Agent Relationship"] = 'Individual'
+e["Solver"]["Multi Agent Correlation"] = False
+e["Solver"]["Strong Truncation Variant"] = True
+
+if(args.model == 1):
+	e["Solver"]["Multi Agent Correlation"] = True
+
+elif(args.model == 2):
+	e["Solver"]["Multi Agent Correlation"] = True
+	e["Solver"]["Strong Truncation Variant"] = False
+
+elif(args.model == 3):
+	e["Solver"]["Multi Agent Relationship"] = 'Cooperation'
+
+elif(args.model == 4):
+	e["Solver"]["Multi Agent Relationship"] = 'Cooperation'
+	e["Solver"]["Multi Agent Correlation"] = True
+
+elif(args.model == 5):
+	e["Solver"]["Multi Agent Relationship"] = 'Cooperation'
+	e["Solver"]["Multi Agent Correlation"] = True
+	e["Solver"]["Strong Truncation Variant"] = False
+
+### Setting Experience Replay and REFER settings
+
+e["Solver"]["Experience Replay"]["Start Size"] = 16384
+e["Solver"]["Experience Replay"]["Maximum Size"] = 262144
+e["Solver"]["Experience Replay"]["Off Policy"]["Annealing Rate"] = 5.0e-8
+e["Solver"]["Experience Replay"]["Off Policy"]["Cutoff Scale"] = 4.0
+e["Solver"]["Experience Replay"]["Off Policy"]["REFER Beta"] = 0.3
+e["Solver"]["Experience Replay"]["Off Policy"]["Target"] = 0.1
+
+e["Solver"]["State Rescaling"]["Enabled"] = True
+e["Solver"]["Reward"]["Rescaling"]["Enabled"] = True
+  
+### Configuring the neural network and its hidden layers
+## TODO: try RNN
+
+e["Solver"]["Neural Network"]["Engine"] = "OneDNN"
+e["Solver"]['Neural Network']['Optimizer'] = "Adam"
+e["Solver"]["L2 Regularization"]["Enabled"] = args.l2 > 0.
+e["Solver"]["L2 Regularization"]["Importance"] = args.l2
+
+
+#e["Solver"]["Neural Network"]["Hidden Layers"][0]["Type"] = "Layer/Recurrent/GRU"
+#e["Solver"]["Neural Network"]["Hidden Layers"][0]["Depth"] = 1
+#e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 64
+
+e["Solver"]["Neural Network"]["Hidden Layers"][0]["Type"] = "Layer/Linear"
+e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 256
+
+e["Solver"]["Neural Network"]["Hidden Layers"][1]["Type"] = "Layer/Activation"
+e["Solver"]["Neural Network"]["Hidden Layers"][1]["Function"] = "Elementwise/Tanh"
+
+e["Solver"]["Neural Network"]["Hidden Layers"][2]["Type"] = "Layer/Linear"
+e["Solver"]["Neural Network"]["Hidden Layers"][2]["Output Channels"] = 256
+
+e["Solver"]["Neural Network"]["Hidden Layers"][3]["Type"] = "Layer/Activation"
+e["Solver"]["Neural Network"]["Hidden Layers"][3]["Function"] = "Elementwise/Tanh"
+
+### Setting file output configuration
+
+e["Solver"]["Termination Criteria"]["Max Experiences"] = 10e6 
+e["Solver"]["Experience Replay"]["Serialize"] = True
+e["Console Output"]["Verbosity"] = "Detailed"
+e["File Output"]["Enabled"] = True
+e["File Output"]["Frequency"] = 500
+e["File Output"]["Path"] = resultFolder
+
+### Running Experiment
+
+k.run(e)
+
+file = open(resultFolder + 'args.txt',"w")
+file.write(str(args))
+file.close()
